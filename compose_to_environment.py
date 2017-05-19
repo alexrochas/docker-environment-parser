546afd418b0fbe1f5ef258d07b01d49a152acbcc
Alex Rocha <alex.rochas@yahoo.com.br>
2017-05-19 15:37:26 -0300

    feat: add parses draft

---
+++ compose_to_environment.py
@@ -0,0 +1,20 @@
+import yaml
+import argparse
+
+
+def parse(container, docker_file):
+    with open(docker_file) as content:
+        container = container
+        parsedYaml = yaml.load(content)
+        environment = parsedYaml[container]['environment']
+
+        for k, v in environment.items():
+            print("export {}={}".format(k, v))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--docker-file')
+    parser.add_argument('-c', '--container')
+    args = parser.parse_args()
+    parse(args.container, args.docker_file)

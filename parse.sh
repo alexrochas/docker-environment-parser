3e61459f2e89cccbe96832c11e0810f6e06a5ded
Alex Rocha <alex.rochas@yahoo.com.br>
2017-05-19 21:44:01 -0300

    feat: add bash script to initialize parse

---
+++ parse.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+usage() { { echo "Usage: $0 [-d <docker-file-path>] [-c <container>]" 1>&2; exit 1; } }
+
+WORK_PATH=`dirname "$0"`
+
+while getopts "d:c:-:" option; do
+	case "${option}" in
+		-) case "${OPTARG}" in
+			docker=*)
+				val=${OPTARG#*=}
+				opt=${OPTARG%=$val}
+				DOCKER_FILE=${val}
+				;;
+			container=*)
+				val=${OPTARG#*=}
+				opt=${OPTARG%=$val}
+				CONTAINER=${val}
+				;;
+			*) usage;;
+		esac;;
+		d) DOCKER_FILE=${OPTARG};;
+		c) CONTAINER=${OPTARG};;
+		*) usage;;
+	esac
+done
+
+function _search(){
+	eval $(python $WORK_PATH/compose_to_environment.py -c $CONTAINER -d $DOCKER_FILE)
+}
+
+_search
